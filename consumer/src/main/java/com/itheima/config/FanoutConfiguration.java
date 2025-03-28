42cacc98094b26b8fe503226b7e232386fbe4f83
hbycgithub2 <1311768852@qq.com>
2025-03-28 11:05:55 +0800

    MQ基础6-Java客户端-声明队列和交换机的方式一

---
+++ consumer/src/main/java/com/itheima/config/FanoutConfiguration.java
@@ -0,0 +1,37 @@
+package com.itheima.config;
+
+import com.rabbitmq.client.AMQP;
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FanoutConfiguration {
+    @Bean
+    public FanoutExchange fanoutExchange() {
+        //ExchangeBuilder.fanoutExchange("").build();
+        return new FanoutExchange("hmall.fanout2");
+    }
+
+    @Bean
+    public Queue fanoutQueue3() {
+        //QueueBuilder.durable("ff").build();
+        return new Queue("fanout.queue3");
+    }
+
+    @Bean
+    public Binding fanoutBinding3(Queue fanoutQueue3, FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(fanoutQueue3).to(fanoutExchange);
+    }
+
+    @Bean
+    public Queue fanoutQueue4() {
+        //QueueBuilder.durable("ff").build();
+        return new Queue("fanout.queue4");
+    }
+
+    @Bean
+    public Binding fanoutBinding4(Queue fanoutQueue3, FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(fanoutQueue4()).to(fanoutExchange);
+    }
+}
