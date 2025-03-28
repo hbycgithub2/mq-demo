f0f19a8d57321796055455def1331d99e3980353
hbycgithub2 <1311768852@qq.com>
2025-03-28 11:42:00 +0800

    MQ基础6-Java客户端-声明队列和交换机的方式二

--- consumer/src/main/java/com/itheima/test/MqListener.java
+++ consumer/src/main/java/com/itheima/test/MqListener.java
@@ -2,6 +2,10 @@ package com.itheima.test;
 
 import com.itheima.consumer.ConsumerApplication;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -37,13 +41,33 @@ public class MqListener {
         Thread.sleep(200);
     }
 
-    @RabbitListener(queues = "direct.queue1")
+    /*@RabbitListener(queues = "direct.queue1")
     public void listenDirectQueue1(String msg) throws InterruptedException {
         System.out.println("消费者1 收到了direct.queue1的消息：[" + msg + "]");
         Thread.sleep(20);
     }
 
     @RabbitListener(queues = "direct.queue2")
+    public void listenDirectQueue2(String msg) throws InterruptedException {
+        System.out.println("消费者2 收到了direct.queue2的消息：[" + msg + "]");
+        Thread.sleep(200);
+    }*/
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "direct.queue1", durable = "true"),
+            exchange = @Exchange(name = "hmall.direct", type = ExchangeTypes.DIRECT),
+            key = {"red", "blue"}
+    ))
+    public void listenDirectQueue1(String msg) throws InterruptedException {
+        System.out.println("消费者1 收到了direct.que ue1的消息：[" + msg + "]");
+        Thread.sleep(20);
+    }
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "direct.queue2", durable = "true"),
+            exchange = @Exchange(name = "hmall.direct", type = ExchangeTypes.DIRECT),
+            key = {"red", "yellow"}
+    ))
     public void listenDirectQueue2(String msg) throws InterruptedException {
         System.out.println("消费者2 收到了direct.queue2的消息：[" + msg + "]");
         Thread.sleep(200);
